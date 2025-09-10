36dc2f07aac780bc537cf522d64576f99307888c
Sardar Ali <sardaralikhamosh@gmail.com>
2025-09-10 19:57:10 +0500

    php
    updated php code with latest content

---
+++ announcement-moving-text.php
@@ -0,0 +1,71 @@
+<div class="infinite-marquee">
+    
+    <div class="marquee-content" aria-hidden="true">
+        <label style="color:#fff; font-weight: 700; font-size:24px; line-height: 40px;">
+            <span class="blink">Update!</span> 
+            <a href="https://shadfoundation.org.pk/girls-hostel-admissions" target="_blank">
+                Admissions Open 2025 | Shad Foundation Girls Hostels in Islamabad, Rawalpindi, and Chitral
+            </a>
+           <span class="blink">NEW!</span> 
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSeQJVX1RKB3cbBondon1m7fpf0Q7K-E3k9nPCZ82eJ_yrbt_w/viewform" target="_blank">
+                Hiring a Taekwondo Trainer – Click Here to Apply Online
+            </a>
+            &nbsp; | &nbsp;
+            <span class="blink">URGENT!</span> 
+            <a href="https://shadfoundation.org.pk/donate" target="_blank">
+                Flood Relief Appeal – GB.
+            </a>
+        </label>
+    </div>
+</div>
+
+<style>
+     .marquee-content {
+        display: flex;
+        align-items: center;
+        white-space: nowrap;
+        font-size: 18px;
+        font-weight: 500;
+    }
+    /* Blinking highlight */
+    .blink {
+        color: red;
+        font-weight: bold;
+        margin-right: 10px;
+        animation: blink 1s infinite;
+    }
+
+.custom-moving {
+    font-family: Helvetica, sans-serif, !important;
+    line-height: 40px;
+}
+.infinite-marquee {
+    width: 100%;
+    overflow: hidden;
+    display: flex;
+    white-space: nowrap;
+    box-sizing: border-box;
+    background: transparent; /* Change as needed */
+}
+
+.marquee-content {
+    display: inline-block;
+    animation: marquee 30s linear infinite;
+    padding-right: 10px; /* Adjust spacing between copies */
+}
+
+.marquee-content label {
+    font-family: Helvetica, sans-serif, !important;
+    display: inline-block;
+    padding-right: 50px; /* Adjust spacing between items */
+}
+
+@keyframes marquee {
+    0% {
+        transform: translateX(0);
+    }
+    100% {
+        transform: translateX(-50%);
+    }
+}
+</style>
